37849160ec37a6b7d369cb3f3a14043419e2c473
anoromi <and.zagorulko@gmail.com>
2025-09-07 15:30:10 +0300

    test

--- examples/run_generic.rs
+++ examples/run_generic.rs
@@ -1,7 +1,10 @@
 use std::{panic::catch_unwind, thread, time::Duration};
 
 use tracing::Level;
-use whatawhat_lib::{GenericWindowManager, WindowManager as _};
+use whatawhat_lib::{
+    GenericWindowManager, WindowManager as _,
+    config::{WatcherConfig, WatcherConfigBuilder},
+};
 
 // #[tokio::main]
 fn main() {
@@ -12,23 +15,28 @@ fn main() {
     //             .build()
     //             .unwrap()
     //             .block_on(async {
-                    let mut window_manager =
-                        GenericWindowManager::new(Duration::from_secs(10), None).unwrap();
+    let mut window_manager = GenericWindowManager::new(
+        WatcherConfigBuilder::default()
+            .idle_timeout(Duration::from_secs(10))
+            .build()
+            .unwrap(),
+    )
+    .unwrap();
 
-                    tracing_subscriber::fmt()
-                        // all spans/events with a level higher than TRACE (e.g, info, warn, etc.)
-                        // will be written to stdout.
-                        .with_max_level(Level::DEBUG)
-                        // sets this to be the default, global subscriber for this application.
-                        .init();
+    tracing_subscriber::fmt()
+        // all spans/events with a level higher than TRACE (e.g, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::DEBUG)
+        // sets this to be the default, global subscriber for this application.
+        .init();
 
-                    loop {
-                        let active_window = window_manager.get_active_window_data();
-                        println!("Active window: {:?}", active_window);
-                        let idle_time = window_manager.is_idle();
-                        println!("Idle time: {:?}", idle_time);
-                        std::thread::sleep(std::time::Duration::from_secs(1));
-                    }
+    loop {
+        let active_window = window_manager.get_active_window_data();
+        println!("Active window: {:?}", active_window);
+        let idle_time = window_manager.is_idle();
+        println!("Idle time: {:?}", idle_time);
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
     //             });
     //     });
     //     dbg!(&result);
--- src/macos.rs
+++ src/macos.rs
@@ -1,4 +1,10 @@
-use std::{process::{Command, Stdio}, str::Lines, time::Duration};
+use std::{
+    io::{BufRead as _, BufReader},
+    process::{Child, Command, Stdio},
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
 use anyhow::{Result, anyhow};
 use objc2::{AllocAnyThread, rc::Retained};
@@ -7,12 +13,11 @@ use objc2_foundation::{NSString, ns_string};
 use objc2_osa_kit::{OSALanguage, OSAScript};
 use serde::{Deserialize, Serialize};
 use sysinfo::{self};
-use tracing::debug;
 
 use super::ActiveWindowData;
 use crate::{WindowManager, config::WatcherConfig};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct AppInfo {
     unix_id: u32,
@@ -24,67 +29,67 @@ struct AppInfo {
 /// synchronously for each get_active_window_data() call. No background threads.
 pub struct MacosManger {
     sysinfo: sysinfo::System,
-    script: Retained<OSAScript>,
+    runner: MacosRunner,
+    // script: Retained<OSAScript>,
     idle_timeout: Duration,
-    on_main_thread: bool,
 }
 
 impl MacosManger {
     pub fn new(config: WatcherConfig) -> Result<Self> {
-        // Prepare OSAScript with JavaScript (JXA)
-        let script = OSAScript::alloc();
-        let language = unsafe { OSALanguage::languageForName(&NSString::from_str("JavaScript")) }
-            .ok_or_else(|| anyhow!("Failed to get JavaScript OSALanguage"))?;
-        let script = unsafe {
-            OSAScript::initWithSource_language(
-                script,
-                ns_string!(include_str!("./print_app_status.jxa")),
-                Some(&language),
-            )
+        let runner = if config.am_on_main_thread {
+            create_on_main_thread_osascript_process()?
+        } else {
+            create_separate_osascript_process(config.idle_check_interval)?
         };
-
-        // Compile once up front
-        let mut err: Option<_> = None;
-        let _ = unsafe { script.compileAndReturnError(err.as_mut()) };
-        if let Some(err) = err {
-            return Err(anyhow!("compile error: {:?}", &err));
-        }
 
         Ok(Self {
             sysinfo: sysinfo::System::new_all(),
-            script,
+            runner,
             idle_timeout: config.idle_timeout,
-            on_main_thread: config.am_on_main_thread,
         })
     }
 }
 
 impl WindowManager for MacosManger {
     fn get_active_window_data(&mut self) -> Result<ActiveWindowData> {
-        // Execute compiled script
-        let mut err: Option<_> = None;
-        let data = unsafe { self.script.executeAndReturnError(err.as_mut()) };
-        if let Some(err) = err {
-            return Err(anyhow!("execution error: {:?}", &err));
-        }
-        dbg!("Script output: {:?}", &data);
-        let json = unsafe {
-            data.ok_or_else(|| anyhow!("No result from OSAScript execution"))?
-                .stringValue()
-        }
-        .ok_or_else(|| anyhow!("Script did not return a string value"))?
-        .to_string();
+        let app_info = match &mut self.runner {
+            MacosRunner::OnMainThread { script } => {
+                // Execute compiled script
+                let mut err: Option<_> = None;
+                let data = unsafe { script.executeAndReturnError(err.as_mut()) };
+                if let Some(err) = err {
+                    return Err(anyhow!("execution error: {:?}", &err));
+                }
+                dbg!("Script output: {:?}", &data);
+                let json = unsafe {
+                    data.ok_or_else(|| anyhow!("No result from OSAScript execution"))?
+                        .stringValue()
+                }
+                .ok_or_else(|| anyhow!("Script did not return a string value"))?
+                .to_string();
 
-        dbg!("Script output: {}", &json);
-        // Parse JXA output
-        let app_info: AppInfo = serde_json::from_str(&json)
-            .map_err(|e| anyhow!("Failed to parse JXA JSON: {e}; payload: {json}"))?;
+                dbg!("Script output: {}", &json);
+                // Parse JXA output
+                let app_info: AppInfo = serde_json::from_str(&json)
+                    .map_err(|e| anyhow!("Failed to parse JXA JSON: {e}; payload: {json}"))?;
+                app_info
+            }
+            MacosRunner::SeparateProcess {
+                current_app_info, ..
+            } => {
+                let app_info = current_app_info.lock().unwrap();
+                let Some(app_info) = app_info.as_ref() else {
+                    return Err(anyhow!("No app info was loaded"));
+                };
+                app_info.clone()
+            }
+        };
 
+        let pid = sysinfo::Pid::from_u32(app_info.unix_id);
         // Resolve process path via sysinfo
         // Refresh processes to ensure info is up to date
         self.sysinfo
-            .refresh_processes(sysinfo::ProcessesToUpdate::All, true);
-        let pid = sysinfo::Pid::from_u32(app_info.unix_id);
+            .refresh_processes(sysinfo::ProcessesToUpdate::Some(&[pid]), true);
         let path = self.sysinfo.process(pid);
         let process_path = match path.and_then(|p| p.exe()) {
             Some(path) => path.to_str().map(|s| s.to_string()),
@@ -113,25 +118,176 @@ impl WindowManager for MacosManger {
 
 enum MacosRunner {
     SeparateProcess {
-        command: Command,
-        // stdout: Lines<BufReader<Stdio>>,
+        process: Child,
+        _handle: thread::JoinHandle<Result<()>>,
+        stop_signal: std::sync::mpsc::Sender<()>,
+        current_app_info: Arc<Mutex<Option<AppInfo>>>,
     },
     OnMainThread {
         script: Retained<OSAScript>,
     },
 }
 
-fn create_osascript_command(duration: Duration) {
+fn create_on_main_thread_osascript_process() -> Result<MacosRunner> {
+    // Prepare OSAScript with JavaScript (JXA)
+    let script = OSAScript::alloc();
+    let language = unsafe { OSALanguage::languageForName(&NSString::from_str("JavaScript")) }
+        .ok_or_else(|| anyhow!("Failed to get JavaScript OSALanguage"))?;
+    let script = unsafe {
+        OSAScript::initWithSource_language(
+            script,
+            ns_string!(include_str!("./print_app_status.jxa")),
+            Some(&language),
+        )
+    };
 
+    // Compile once up front
+    let mut err: Option<_> = None;
+    let _ = unsafe { script.compileAndReturnError(err.as_mut()) };
+    if let Some(err) = err {
+        return Err(anyhow!("compile error: {:?}", &err));
+    }
+
+    Ok(MacosRunner::OnMainThread { script })
 }
 
-// fn create_separate_process() -> Result<MacosRunner> {
-//     let command = Command::new("osascript")
-//         .stdout(Stdio::piped())
-//         .arg("-e")
-//         .arg(COMMAND)
-//         .arg("-l")
-//         .arg("JavaScript")
-//         .spawn()
-//         .unwrap();
-// }
+fn create_separate_osascript_process(collection_interval: Duration) -> Result<MacosRunner> {
+    let current_app_info = Arc::new(Mutex::new(None));
+    let inner_current_app_info = current_app_info.clone();
+
+    #[allow(
+        clippy::zombie_processes,
+        reason = "Process is killed by the Drop impl"
+    )]
+    let mut process = Command::new("osascript")
+        .stdout(Stdio::piped())
+        .arg("-e")
+        .arg(create_osascript_command(collection_interval))
+        .arg("-l")
+        .arg("JavaScript")
+        .spawn()
+        .unwrap();
+
+    let stdout = process.stdout.take().expect("Stdout was not piped");
+    let (stop_signal, stop_signal_receiver) = std::sync::mpsc::channel();
+    let handle = thread::spawn(move || {
+        let lines = BufReader::new(stdout).lines();
+        for line in lines {
+            if stop_signal_receiver.try_recv().is_ok() {
+                return Ok(());
+            }
+            let line = line.unwrap();
+            let app_info: AppInfo = serde_json::from_str(&line).unwrap();
+            let mut current_app_info = inner_current_app_info.lock().unwrap();
+            *current_app_info = Some(app_info);
+        }
+        Ok(())
+    });
+    Ok(MacosRunner::SeparateProcess {
+        process,
+        _handle: handle,
+        stop_signal,
+        current_app_info,
+    })
+}
+
+impl Drop for MacosRunner {
+    fn drop(&mut self) {
+        match self {
+            MacosRunner::SeparateProcess {
+                process,
+                stop_signal,
+                ..
+            } => {
+                let _ = stop_signal.send(());
+                let _ = process.kill();
+            }
+            MacosRunner::OnMainThread { .. } => {}
+        }
+    }
+}
+
+fn create_osascript_command(collection_interval: Duration) -> String {
+    format!(
+        r#"
+#!/usr/bin/osascript -l JavaScript
+
+// adapted from:
+// https://gist.github.com/EvanLovely/cb01eafb0d61515c835ecd56f6ac199a
+
+// new to jxa?
+// - https://apple-dev.groups.io/g/jxa/wiki/3202
+// - interactive repl: `osascript -il JavaScript`
+// - API reference: Script Editor -> File -> Open Dictionary
+
+function getApp() {{
+  var seApp = Application("System Events")
+  var oProcess = seApp.processes.whose({{ frontmost: true }})[0]
+  var appName = oProcess.displayedName()
+
+  // as of 05/01/21 incognio & url are not actively used in AW
+  // variables must be set to `undefined` since this script is re-run via osascript
+  // and the previously set values will be cached otherwise
+  var url = undefined,
+    incognito = undefined,
+    title = undefined
+
+  // it's not possible to get the URL from firefox
+  // https://stackoverflow.com/questions/17846948/does-firefox-offer-applescript-support-to-get-url-of-windows
+
+  switch (appName) {{
+    case "Safari":
+      // incognito is not available via safari applescript
+      url = Application(appName).documents[0].url()
+      title = Application(appName).documents[0].name()
+      break
+    case "Google Chrome":
+    case "Google Chrome Canary":
+    case "Chromium":
+    case "Brave Browser":
+      const activeWindow = Application(appName).windows[0]
+      const activeTab = activeWindow.activeTab()
+
+      url = activeTab.url()
+      title = activeTab.name()
+      incognito = activeWindow.mode() === "incognito"
+      break
+    case "Firefox":
+    case "Firefox Developer Edition":
+      title = Application(appName).windows[0].name()
+      break
+    default:
+      mainWindow = oProcess
+        .windows()
+        .find((w) => w.attributes.byName("AXMain").value() === true)
+
+      // in some cases, the primary window of an application may not be found
+      // this occurs rarely and seems to be triggered by switching to a different application
+      if (mainWindow) {{
+        title = mainWindow.attributes.byName("AXTitle").value()
+      }}
+  }}
+
+  // key names must match expected names in lib.py
+  return JSON.stringify({{
+    app: appName,
+    url,
+    title,
+    incognito,
+  }})
+}}
+
+var seApp = Application("System Events")
+function runCollector() {{
+  while (true) {{
+    console.log(getApp())
+    delay({})
+  }}
+}}
+
+runCollector()
+
+    "#,
+        collection_interval.as_secs()
+    )
+}
