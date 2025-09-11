d0538077b5cf54bbe88df6a121a3459abbfbd096
Anoromi <and.zagorulko@gmail.com>
2025-09-11 10:02:58 +0300

    test

--- src/utils.rs
+++ src/utils.rs
@@ -6,6 +6,8 @@ use crate::simple_cache::CacheConfig;
 
 pub fn is_gnome() -> bool {
     // Electron updates the XDG_CURRENT_DESKTOP for Chromium, so we need to check the other value.
+    info!("ORIGINAL_XDG_CURRENT_DESKTOP {:?}", std::env::var("ORIGINAL_XDG_CURRENT_DESKTOP"));
+    info!("XDG_CURRENT_DESKTOP {:?}", std::env::var("ORIGINAL_XDG_CURRENT_DESKTOP"));
     if let Ok(de) = std::env::var("ORIGINAL_XDG_CURRENT_DESKTOP") {
         de.to_lowercase().contains("gnome")
     } else if let Ok(de) = std::env::var("XDG_CURRENT_DESKTOP") {
